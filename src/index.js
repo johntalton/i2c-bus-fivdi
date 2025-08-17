ba72a5bd0329fce2dc118fede56023e97d9ae81c
johntalton <johntalton@gmail.com>
2025-08-17 01:59:01 -0400

    typing and buffer wrapped

--- src/index.js
+++ src/index.js
@@ -1,65 +1,120 @@
 import * as i2c from 'i2c-bus'
 
+/** @import { I2CBus, I2CBufferSource, I2CReadResult, I2CWriteResult, I2CAddress } from '@johntalton/and-other-delights' */
+
 const BASE_NAME = 'Fivdi'
 
+
 export class FivdiBusProvider {
-  static async openPromisified(busNumber) {
-    return new FivdiBus(await i2c.openPromisified(busNumber), `${BASE_NAME}-${busNumber}`)
-  }
+	static async openPromisified(busNumber) {
+		return new FivdiBus(await i2c.openPromisified(busNumber), `${BASE_NAME}-${busNumber}`)
+	}
 }
 
+/**
+ * @implements {I2CBus}
+ */
 export class FivdiBus {
-  #bus
-  #name
+	/** @type {i2c.PromisifiedBus} */
+	#bus
+	#name
 
-  constructor(i2cBus, name = BASE_NAME) {
-    this.#name = name
-    this.#bus = i2cBus
-  }
+	/**
+	 * @param {i2c.PromisifiedBus} i2cBus
+	 */
+	constructor(i2cBus, name) {
+		this.#name = name ?? BASE_NAME
+		this.#bus = i2cBus
+	}
 
-  get name() { return this.#name }
+	get supportsScan() { return true }
+	get supportsMultiByteDataAddress() { return false }
 
-  close() { this.#bus.closeSync() }
+	get name() { return this.#name }
 
-  sendByte(address, byteValue) {
-    return this.#bus.sendByte(address, byteValue)
-  }
+	close() { this.#bus.close() }
 
-  async readI2cBlock(address, cmd, length, bufferSource) {
-    const intoBuffer = Buffer.from(bufferSource)
-    const { bytesRead, buffer } = await this.#bus.readI2cBlock(address, cmd, length, intoBuffer)
-    return {
-      bytesRead,
-      buffer: buffer.buffer
-    }
-  }
+	/**
+	 * @param {I2CAddress} address
+	 * @param {number} byteValue
+	 * @returns {Promise<void>}
+	 */
+	sendByte(address, byteValue) {
+		return this.#bus.sendByte(address, byteValue)
+	}
 
-  async writeI2cBlock(address, cmd, length, bufferSource) {
-    const bufferToWrite = Buffer.from(bufferSource)
-    const { bytesWritten, buffer } = await this.#bus.writeI2cBlock(address, cmd, length, bufferToWrite)
-    return {
-      bytesWritten,
-      buffer: buffer.buffer
-    }
-  }
+	/**
+	 * @param {I2CAddress} address
+	 * @param {number} cmd
+	 * @param {number} length
+	 * @param {I2CBufferSource} bufferSource
+	 * @returns {Promise<I2CReadResult>}
+	 */
+	async readI2cBlock(address, cmd, length, bufferSource) {
+		const intoBuffer = ArrayBuffer.isView(bufferSource) ?
+			Buffer.from(bufferSource.buffer, bufferSource.byteOffset, bufferSource.byteLength) :
+			Buffer.from(bufferSource)
+		const { bytesRead, buffer } = await this.#bus.readI2cBlock(address, cmd, length, intoBuffer)
+		return {
+			bytesRead,
+			buffer: new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+		}
+	}
 
-  async i2cRead(address, length, bufferSource) {
-    const intoBuffer = Buffer.from(bufferSource)
-    const { bytesRead, buffer } = await this.#bus.i2cRead(address,length, intoBuffer)
-    return {
-      bytesRead,
-      buffer: buffer.buffer
-    }
-  }
+	/**
+	 * @param {I2CAddress} address
+	 * @param {number} cmd
+	 * @param {number} length
+	 * @param {I2CBufferSource} bufferSource
+	 * @returns {Promise<I2CWriteResult>}
+	 */
+	async writeI2cBlock(address, cmd, length, bufferSource) {
+		const bufferToWrite = ArrayBuffer.isView(bufferSource) ?
+			Buffer.from(bufferSource.buffer, bufferSource.byteOffset, bufferSource.byteLength) :
+			Buffer.from(bufferSource)
+		const { bytesWritten, buffer } = await this.#bus.writeI2cBlock(address, cmd, length, bufferToWrite)
+		return {
+			bytesWritten,
+			buffer: new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+		}
+	}
 
-  async i2cWrite(address, length, bufferSource) {
-    const bufferToWrite = Buffer.from(bufferSource)
-    const { bytesWritten, buffer } = await this.#bus.i2cWrite(address, length, bufferToWrite)
-    return {
-      bytesWritten,
-      buffer: buffer.buffer
-    }
-  }
+	/**
+	 * @param {I2CAddress} address
+	 * @param {number} length
+	 * @param {I2CBufferSource} bufferSource
+	 * @returns {Promise<I2CReadResult>}
+	 */
+	async i2cRead(address, length, bufferSource) {
+		const intoBuffer = ArrayBuffer.isView(bufferSource) ?
+			Buffer.from(bufferSource.buffer, bufferSource.byteOffset, bufferSource.byteLength) :
+			Buffer.from(bufferSource)
+		const { bytesRead, buffer } = await this.#bus.i2cRead(address, length, intoBuffer)
+		return {
+			bytesRead,
+			buffer: new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+		}
+	}
 
-  async scan() { return this.#bus.scan() }
+	/**
+	 * @param {I2CAddress} address
+	 * @param {number} length
+	 * @param {I2CBufferSource} bufferSource
+   * @returns {Promise<I2CWriteResult>}
+   */
+	async i2cWrite(address, length, bufferSource) {
+		const bufferToWrite = ArrayBuffer.isView(bufferSource) ?
+			Buffer.from(bufferSource.buffer, bufferSource.byteOffset, bufferSource.byteLength) :
+			Buffer.from(bufferSource)
+		const { bytesWritten, buffer } = await this.#bus.i2cWrite(address, length, bufferToWrite)
+		return {
+			bytesWritten,
+			buffer: new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+		}
+	}
+
+	/**
+	 * @returns {Promise<I2CAddress[]>}
+	 */
+	async scan() { return this.#bus.scan() }
 }
